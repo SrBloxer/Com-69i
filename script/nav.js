dcbf887cabc6ce1444f45cc2487fcbd79d2afb94
vale <vlatapie2mail.com>
2023-11-21 10:32:31 -0300

    se desarrollo navbar, footer, pagina nosotros

---
+++ script/nav.js
@@ -0,0 +1,35 @@
+ const navContainer = document.getElementById("navContainer");
+
+
+ let urlHome;
+ let urlLogin;
+ let urlRegister;
+ let urlNosotros;
+
+ 
+
+navContainer.innerHTML = `
+<nav class="navbar nav-music  navbar-expand-lg ">
+  <div class="container-fluid ">
+    <a class="navbar-brand" href="#">  <img class="icono-footer" src="../assets/icon/ms-icon-310x310.png"  alt="icono music code" /></a>
+    <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon bg-white"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav  me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Regístrate</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Iniciar sesión</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <button class="btn btn-outline-success bg-white text-dark"  type="submit">Prueba Gratis</button>
+      </form>
+    </div>
+  </div>
+</nav>`
